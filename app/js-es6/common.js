b61d64a34e31b593eb823e95907f447ecd7bd0b3
danil.khandys <danil.khandys@xpage.ru>
2018-06-05 09:57:06 +0500

    Закончил адаптив

--- app/js-es6/common.js
+++ app/js-es6/common.js
@@ -1,91 +1,109 @@
 $(e => {
-	$('#content').fullpage({
-		//Навигация
-		menu: '#menu',
-		lockAnchors: false,
-		anchors:['page-1', 'page-2', "page-3", "page-4", "page-5"],
-		navigation: false,
-		navigationPosition: 'right',
-		navigationTooltips: ['firstSlide', 'secondSlide'],
-		showActiveTooltip: false,
-		slidesNavigation: false,
-		slidesNavPosition: 'bottom',
 
-		//Скроллинг
-		css3: true,
-		scrollingSpeed: 700,
-		autoScrolling: true,
-		fitToSection: true,
-		fitToSectionDelay: 1000,
-		scrollBar: false,
-		easing: 'easeInOutCubic',
-		easingcss3: 'ease',
-		loopBottom: false,
-		loopTop: false,
-		loopHorizontal: true,
-		continuousVertical: false,
-		continuousHorizontal: false,
-		scrollHorizontally: false,
-		interlockedSlides: false,
-		dragAndMove: false,
-		offsetSections: false,
-		resetSliders: false,
-		fadingEffect: false,
-		normalScrollElements: '#element1, .element2',
-		scrollOverflow: false,
-		scrollOverflowReset: false,
-		scrollOverflowOptions: null,
-		touchSensitivity: 15,
-		normalScrollElementTouchThreshold: 5,
-		bigSectionsDestination: null,
+	if (is.ie())
+		$("picture").each((i, el) => {
+			let $this = $(el),
+				$mainSource = $this.find("source[data-main]").length 
+							  ? $this.find("source[data-main]")
+							  : $this.find("source:eq(0)");
 
-		//Доступ
-		keyboardScrolling: true,
-		animateAnchor: true,
-		recordHistory: true,
+			if (!$mainSource.length)
+				return
 
-		//Дизайн
-		controlArrows: true,
-		verticalCentered: true,
-		sectionsColor : ['#ccc', '#fff'],
-		paddingTop: $(".head").innerHeight() + "px",
-		paddingBottom: '0',
-		fixedElements: '.head, .years',
-		responsiveWidth: 600,
-		responsiveHeight: 0,
-		responsiveSlides: false,
-		parallax: false,
-		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+			let imgSrc = $mainSource.attr("srcset");
 
-		//Настроить селекторы
-		sectionSelector: '.page',
-		slideSelector: '.slide',
+			$this.find("img")[0].src = imgSrc;
+		});
 
-		lazyLoading: true,
+	if (!is.mobile()){
+		$('#content').fullpage({
+			//Навигация
+			menu: '#menu',
+			lockAnchors: false,
+			anchors:['page-1', 'page-2', "page-3", "page-4", "page-5"],
+			navigation: false,
+			navigationPosition: 'right',
+			navigationTooltips: ['firstSlide', 'secondSlide'],
+			showActiveTooltip: false,
+			slidesNavigation: false,
+			slidesNavPosition: 'bottom',
 
-		//события
-		onLeave: function(index, nextIndex, direction){},
-		afterLoad: function(anchorLink, index){},
-		afterRender: function(){},
-		afterResize: function(){},
-		afterResponsive: function(isResponsive){},
-		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-	});
+			//Скроллинг
+			css3: true,
+			scrollingSpeed: 700,
+			autoScrolling: true,
+			fitToSection: true,
+			fitToSectionDelay: 1000,
+			scrollBar: false,
+			easing: 'easeInOutCubic',
+			easingcss3: 'ease',
+			loopBottom: false,
+			loopTop: false,
+			loopHorizontal: true,
+			continuousVertical: false,
+			continuousHorizontal: false,
+			scrollHorizontally: false,
+			interlockedSlides: false,
+			dragAndMove: false,
+			offsetSections: false,
+			resetSliders: false,
+			fadingEffect: false,
+			normalScrollElements: '#element1, .element2',
+			scrollOverflow: false,
+			scrollOverflowReset: false,
+			scrollOverflowOptions: null,
+			touchSensitivity: 15,
+			normalScrollElementTouchThreshold: 5,
+			bigSectionsDestination: null,
 
-	$(".page-link--top").click(e => {
-		$.fn.fullpage.moveSectionUp();
-	});
+			//Доступ
+			keyboardScrolling: true,
+			animateAnchor: true,
+			recordHistory: true,
 
-	$(".page-link--bot").click(e => {
-		$.fn.fullpage.moveSectionDown();
-	});
+			//Дизайн
+			controlArrows: true,
+			verticalCentered: true,
+			sectionsColor : ['#ccc', '#fff'],
+			paddingTop: $(".head").innerHeight() + "px",
+			paddingBottom: '0',
+			fixedElements: '.head, .years',
+			responsiveWidth: 600,
+			responsiveHeight: 0,
+			responsiveSlides: false,
+			parallax: false,
+			parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
 
-	$(".page__text .text-page p").each((i, el) => {
-		new stringEffect({
-			selector: $(el),
-		})
-	});
+			//Настроить селекторы
+			sectionSelector: '.page',
+			slideSelector: '.slide',
+
+			lazyLoading: true,
+
+			//события
+			onLeave: function(index, nextIndex, direction){},
+			afterLoad: function(anchorLink, index){},
+			afterRender: function(){},
+			afterResize: function(){},
+			afterResponsive: function(isResponsive){},
+			afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+			onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+		});
+
+		$(".page-link--top").click(e => {
+			$.fn.fullpage.moveSectionUp();
+		});
+
+		$(".page-link--bot").click(e => {
+			$.fn.fullpage.moveSectionDown();
+		});
+
+		$(".page__text .text-page p").each((i, el) => {
+			new stringEffect({
+				selector: $(el),
+			})
+		});
+	}
 });
 
 
@@ -235,3 +253,10 @@ class stringEffect{
 		});
 	}
 }
+
+$(window).on("load scroll", e => {
+	if (window.scrollY >= 190)
+		$(".head").addClass("js__scroll")
+	else
+		$(".head").removeClass("js__scroll")
+});
